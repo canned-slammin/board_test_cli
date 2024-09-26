114647f1116cf247d672d62a38f3a3bd1161ff57
samburner <sd.burner@gmail.com>
2024-09-26 14:54:57 -0700

    defined stm32 board, add TODOs for wireframe test

---
+++ src/cli_commands.c
@@ -0,0 +1,9 @@
+/*
+TODO: test command()
+parse input
+create request message
+create return message
+put request message in queue
+wait for return message in queue
+print return and error
+*/
---
+++ src/cli_sync.c
@@ -0,0 +1,9 @@
+/*
+TODO 
+init?
+enqueue_request
+dequeue_request
+enqueue_return
+dequeue_return
+
+*/
