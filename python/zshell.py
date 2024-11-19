430e5834e5d99a760a89c7af3cc15e50eb240446
Sam Burner <samburner@meta.com>
2024-11-19 12:52:59 -0800

    added beginnings of python interface

---
+++ python/zshell.py
@@ -0,0 +1,92 @@
+
+
+
+class RTTInterface:
+    def __init__(self):
+        print('RTT interface not yet implemented')
+
+    def connect(self):
+        print('not yet implemented')
+
+    def read(self):
+        print('not yet implemented')
+
+    def write(self):
+        print('not yet implemented')
+
+class UARTInterface:
+    def __init__(self):
+        print('UART interface not yet implemented')
+
+    def connect(self):
+        print('not yet implemented')
+
+    def read(self):
+        print('not yet implemented')
+
+    def write(self):
+        print('not yet implemented')
+
+class ZShell:
+    def __init__(self, interface='uart'):
+        if interface.lower() is 'uart':
+            self.interface = UARTInterface()
+        if interface.lower() is 'rtt':
+            self.interface = RTTInterface()
+
+    def connect(self):
+        self.interface.connect()
+
+    def read(self):
+        self.interface.read()
+
+    def write(self):
+        self.interface.write()
+
+    def gpio_conf(self, io='input', pull=None, active=None, init=None, config_flags=None):
+        print('not yet implemented')
+
+    def gpio_get(self, device, pin):
+        print('not yet implemented')
+
+    def gpio_set(self, device, pin, level='0'):
+        print('not yet implemented')
+
+    def gpio_toggle(self, device, pin):
+        print('not yet implemented')
+
+    def gpio_devices(self):
+        print('not yet implemented')
+
+    def gpio_blink(self, device, pin):
+        print('not yet implemented')
+
+    def gpio_info(self, device):
+        print('not yet implemented')
+
+# TODO GPIO
+
+  #conf     : Configure GPIO pin
+  #          Usage: gpio conf <device> <pin> <configuration <i|o>[u|d][h|l][0|1]>
+  #          [vendor specific]
+  #          <i|o> - input|output
+  #          [u|d] - pull up|pull down, otherwise open
+  #          [h|l] - active high|active low, otherwise defaults to active high
+  #          [0|1] - initialise to logic 0|logic 1, otherwise defaults to logic 0
+  #          [vendor specific] - configuration flags within the mask 0xFF00
+  #          see include/zephyr/dt-bindings/gpio/
+  #get      : Get GPIO pin value
+  #          Usage: gpio get <device> <pin>
+  #set      : Set GPIO pin value
+  #          Usage: gpio set <device> <pin> <level 0|1>
+  #toggle   : Toggle GPIO pin
+  #          Usage: gpio toggle <device> <pin>
+  #devices  : List all GPIO devices
+  #          Usage: gpio devices
+  #blink    : Blink GPIO pin
+  #          Usage: gpio blink <device> <pin>
+  #info     : GPIO Information
+  #          Usage: gpio info [device]
+
+
+
